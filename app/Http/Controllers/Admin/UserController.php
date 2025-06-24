2f5cf783eae0eb6a038300d596d6bc970a1fb38a
ROSTAND LELE <rostoinfocus@gmail.com>
2025-06-24 23:56:10 +0100

    -Redirection avec message de succès

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -134,7 +134,7 @@ class UserController extends Controller
             'password' => Hash::make($validated['password']),
         ]);
         
-        // Redirection avec message de succès
+        // -Redirection avec message de succès
         return redirect()->route('users.index')->with('success', 'Utilisateur créé');
     }
 
